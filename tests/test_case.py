8f59148e52bfb200b3d1e70264727c9f3575e5d3
cristi <cvasile@bu.edu>
2015-10-04 23:00:27 -0400

    Created test file for case studies with data loaded from matlab files.

---
+++ tests/test_case.py
@@ -0,0 +1,45 @@
+import numpy as np
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+
+from lltinf import perfect_stop, depth_stop, lltinf, Traces
+
+
+def load_traces(filename):
+    ''' Loads traces' data from a matlab MAT file. The data is stores in 3
+    variables called ``data'', ``labels'' and ``t'' which contain the signals'
+    values for all dimensions, the class labels (-1 and 1) and the times at
+    which the values are sampled.
+    ''' 
+    # load data from MAT file
+    mat_data =  loadmat(filename)
+    # add time dimension to signals' data
+    dims = list(mat_data['data'].shape)
+    dims[1] += 1
+    data = np.zeros(dims)
+    data[:, :dims[1]-1, :] = mat_data['data']
+    data[:, dims[1]-1, :] = mat_data['t']
+    # create list of labels (classes: positive, negative)
+    labels = list(mat_data['labels'][0])
+    # create data structure of traces
+    return Traces(signals=data, labels=labels)
+
+def lltinf_simple_test():
+    '''Simple case study.'''
+    traces = load_traces(r'..\Test Cases\SimpleDS\SimpleDS.mat')
+    print traces.signals.shape
+    print len(traces.labels)
+    # run classification algorithm
+    lltinf(traces, depth=1, stop_condition=[perfect_stop, depth_stop])
+
+def lltinf_naval_test():
+    '''Naval vessel case study.'''
+    traces = load_traces(r'..\Test Cases\Naval\naval_preproc_data_online.mat')
+    print traces.signals.shape
+    print len(traces.labels)
+    # run classification algorithm
+    lltinf(traces, depth=1, stop_condition=[perfect_stop, depth_stop])
+
+if __name__ == '__main__':
+    lltinf_simple_test()
+    lltinf_naval_test()
