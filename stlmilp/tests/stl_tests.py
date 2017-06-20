503e9b4be952a3f5b73770c5382ee741ff03ad35
Francisco Penedo <fran.penedo@gmail.com>
2017-06-20 10:24:49 -0400

    Reworked stl tests

--- stlmilp/tests/stl_tests.py
+++ stlmilp/tests/stl_tests.py
@@ -1,78 +1,73 @@
-from ..stl import *
+import logging
+import unittest
 
+import stlmilp.stl as stl
 
-def test_horizon():
-    f = Formula(NEXT, [
-        Formula(ALWAYS, bounds=[3, 5], args=[
-            Formula(EVENTUALLY, bounds=[2, 4], args=[
-                Formula(AND, args=[
-                    Formula(OR, args=[
-                        Formula(EXPR, []),
-                        Formula(ALWAYS, bounds=[1, 3],
-                                args=[Formula(EXPR, [])]),
-                        Formula(EVENTUALLY, bounds=[0, 2],
-                                args=[Formula(EXPR, [])])
-                    ]),
-                    Formula(NOT, [Formula(EXPR, [])])
+class TestSTL(unittest.TestCase):
+    def setUp(self):
+        self.f = stl.Formula(stl.NEXT, [
+            stl.Formula(stl.ALWAYS, bounds=[3, 5], args=[
+                stl.Formula(stl.EVENTUALLY, bounds=[2, 4], args=[
+                    stl.Formula(stl.AND, args=[
+                        stl.Formula(stl.OR, args=[
+                            stl.Formula(stl.EXPR, []),
+                            stl.Formula(stl.ALWAYS, bounds=[1, 3],
+                                    args=[stl.Formula(stl.EXPR, [])]),
+                            stl.Formula(stl.EVENTUALLY, bounds=[0, 2],
+                                    args=[stl.Formula(stl.EXPR, [])])
+                        ]),
+                        stl.Formula(stl.NOT, [stl.Formula(stl.EXPR, [])])
+                    ])
                 ])
             ])
         ])
-    ])
+        self.s = [0, 1, 2, 4, 8, 4, 2, 1, 0]
+        class Model():
+            def __init__(self, s):
+                self.s = s
+                self.tinter = 1
 
-    assert f.horizon() == 13
+            def getVarByName(self, j):
+                return self.s[j]
 
+        self.model = Model(self.s)
+        self.labels = [lambda x: x]
 
-s = [0, 1, 2, 4, 8, 4, 2, 1, 0]
-class Model():
-    def __init__(self, s):
-        self.s = s
-        self.tinter = 1
+    def test_horizon(self):
+        assert self.f.horizon() == 13
 
-    def getVarByName(self, j):
-        return self.s[j]
+    def test_robustness_expr(self):
+        f = stl.Formula(stl.EXPR, [stl.Signal(self.labels, lambda x: x[0] + 3)])
+        assert(stl.robustness(f, self.model, 2) == self.s[2] + 3)
 
-model = Model(s)
-labels = [lambda x: x]
+    def test_robustness_not(self):
+        g = stl.Formula(stl.EXPR, [stl.Signal(self.labels, lambda x: x[0] + 3)])
+        f = stl.Formula(stl.NOT, [g])
+        assert(-stl.robustness(f, self.model, 2) == self.s[2] + 3)
 
+    def test_robustness_and(self):
+        g = stl.Formula(stl.EXPR, [stl.Signal(self.labels, lambda x: x[0] + 3)])
+        h = stl.Formula(stl.EXPR, [stl.Signal(self.labels, lambda x: x[0] + 1)])
+        f = stl.Formula(stl.AND, [g, h])
+        assert(stl.robustness(f, self.model, 2) == min(self.s[2] + 3, self.s[2] + 1))
 
-def test_robustness_expr():
-    f = Formula(EXPR, [Signal(labels, lambda x: x[0] + 3)])
-    assert(robustness(f, model, 2) == s[2] + 3)
+    def test_robustness_or(self):
+        g = stl.Formula(stl.EXPR, [stl.Signal(self.labels, lambda x: x[0] + 3)])
+        h = stl.Formula(stl.EXPR, [stl.Signal(self.labels, lambda x: x[0] + 1)])
+        f = stl.Formula(stl.OR, [g, h])
+        assert(stl.robustness(f, self.model, 2) == max(self.s[2] + 3, self.s[2] + 1))
 
+    def test_robustness_next(self):
+        g = stl.Formula(stl.EXPR, [stl.Signal(self.labels, lambda x: x[0] + 3)])
+        f = stl.Formula(stl.NEXT, [g])
+        assert(stl.robustness(f, self.model, 2) == self.s[3] + 3)
 
-def test_robustness_not():
-    g = Formula(EXPR, [Signal(labels, lambda x: x[0] + 3)])
-    f = Formula(NOT, [g])
-    assert(-robustness(f, model, 2) == s[2] + 3)
+    def test_robustness_always(self):
+        g = stl.Formula(stl.EXPR, [stl.Signal(self.labels, lambda x: x[0] + 3)])
+        f = stl.Formula(stl.ALWAYS, [g], [1, 3])
+        assert(stl.robustness(f, self.model, 2) == min(self.s[3] + 3, self.s[4] + 3, self.s[5] + 3))
 
-
-def test_robustness_and():
-    g = Formula(EXPR, [Signal(labels, lambda x: x[0] + 3)])
-    h = Formula(EXPR, [Signal(labels, lambda x: x[0] + 1)])
-    f = Formula(AND, [g, h])
-    assert(robustness(f, model, 2) == min(s[2] + 3, s[2] + 1))
-
-
-def test_robustness_or():
-    g = Formula(EXPR, [Signal(labels, lambda x: x[0] + 3)])
-    h = Formula(EXPR, [Signal(labels, lambda x: x[0] + 1)])
-    f = Formula(OR, [g, h])
-    assert(robustness(f, model, 2) == max(s[2] + 3, s[2] + 1))
-
-
-def test_robustness_next():
-    g = Formula(EXPR, [Signal(labels, lambda x: x[0] + 3)])
-    f = Formula(NEXT, [g])
-    assert(robustness(f, model, 2) == s[3] + 3)
-
-
-def test_robustness_always():
-    g = Formula(EXPR, [Signal(labels, lambda x: x[0] + 3)])
-    f = Formula(ALWAYS, [g], [1, 3])
-    assert(robustness(f, model, 2) == min(s[3] + 3, s[4] + 3, s[5] + 3))
-
-
-def test_robustness_eventually():
-    g = Formula(EXPR, [Signal(labels, lambda x: x[0] + 3)])
-    f = Formula(EVENTUALLY, [g], [1, 3])
-    assert(robustness(f, model, 2) == max(s[3] + 3, s[4] + 3, s[5] + 3))
+    def test_robustness_eventually(self):
+        g = stl.Formula(stl.EXPR, [stl.Signal(self.labels, lambda x: x[0] + 3)])
+        f = stl.Formula(stl.EVENTUALLY, [g], [1, 3])
+        assert(stl.robustness(f, self.model, 2) == max(self.s[3] + 3, self.s[4] + 3, self.s[5] + 3))
