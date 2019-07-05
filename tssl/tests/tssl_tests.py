8b7f384b1720e591d211f4d7706e5c556817e7a6
Francisco Penedo <fran.penedo@gmail.com>
2019-07-05 14:32:06 -0400

    Added tssl robustness test

--- tssl/tests/tssl_tests.py
+++ tssl/tests/tssl_tests.py
@@ -12,8 +12,8 @@ FOCUSED = os.environ.get("FOCUSED", False)
 
 
 class TestTSSL(unittest.TestCase):
-    def test_formula(self):
-        f = tssl.TSSLAnd(
+    def setUp(self):
+        self.f = tssl.TSSLAnd(
             [
                 tssl.TSSLOr(
                     [
@@ -30,5 +30,16 @@ class TestTSSL(unittest.TestCase):
                     [tssl.Direction.SW, tssl.Direction.SE],
                     tssl.TSSLPred([-1], 1, tssl.Relation.LE),
                 ),
+                tssl.TSSLTop(),
             ]
         )
+
+    def test_formula_str(self):
+        s = "((¬ E_{NW,NE} X ([1]' x - 0 > 0) v _|_) ^ A_{SW,SE} X ([-1]' x - 1 < 0) ^ T)"
+        npt.assert_equal(str(self.f), s)
+
+    def test_robustness(self):
+        qt = quadtree.QuadTree.from_matrix([[0, 1], [2, 3]], np.mean)
+        model = tssl.TSSLModel(qt, 4)
+        npt.assert_equal(self.f.robustness(model), -0.25)
+        npt.assert_equal(self.f._args[1].robustness(model), 0.75)
