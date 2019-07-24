e3b8f992676867f852b575f8997722948e96870f
Francisco Penedo <fran.penedo@gmail.com>
2019-07-24 16:53:55 -0400

    Updated requirements

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'STL MILP tools',
-    'url': '',
-    'author': 'Fran Penedo',
-    'author_email': 'franp@bu.edu',
-    'version': '0.1.1',
-    'install_requires': ['nose'],
-    'packages': ['stlmilp'],
-    'scripts': [],
-    'name': 'stlmilp'
+    "description": "STL MILP tools",
+    "url": "",
+    "author": "Fran Penedo",
+    "author_email": "franp@bu.edu",
+    "version": "1.0.1",
+    "install_requires": ["numpy>=1.16.4", "pyparsing>=2.1.0", "gurobipy>=7.0.2"],
+    "packages": ["stlmilp"],
+    "scripts": [],
+    "name": "stlmilp",
 }
 
 setup(**config)
