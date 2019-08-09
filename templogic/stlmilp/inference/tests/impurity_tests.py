9da42674715873bc6fa17b3a44bf8bf9c458527a
Francisco Penedo <fran.penedo@gmail.com>
2019-08-09 14:21:38 -0400

    Fixed imports in tests

--- templogic/stlmilp/inference/tests/impurity_tests.py
+++ templogic/stlmilp/inference/tests/impurity_tests.py
@@ -6,17 +6,21 @@ import unittest
 import numpy as np
 import numpy.testing as npt
 
-from lltinf import inference, impurity, llt
+from .. import inference, impurity
+from ... import llt
+
 
 class TestSTL(unittest.TestCase):
-
     def test_ext_inf_gain(self):
-        traces = inference.Traces([
-            [[1,2,3,4], [1,2,3,3.0], [1,2,3,4]],
-            [[1,2,3,4], [1,2,3,3.5], [1,2,3,4]],
-            [[1,2,3,4], [1,2,3,5.0], [1,2,3,4]],
-            [[1,2,3,4], [1,2,3,6.0], [1,2,3,4]]
-        ], [1, 1, -1, -1])
+        traces = inference.Traces(
+            [
+                [[1, 2, 3, 4], [1, 2, 3, 3.0], [1, 2, 3, 4]],
+                [[1, 2, 3, 4], [1, 2, 3, 3.5], [1, 2, 3, 4]],
+                [[1, 2, 3, 4], [1, 2, 3, 5.0], [1, 2, 3, 4]],
+                [[1, 2, 3, 4], [1, 2, 3, 6.0], [1, 2, 3, 4]],
+            ],
+            [1, 1, -1, -1],
+        )
         models = [llt.SimpleModel(signal) for signal in traces.signals]
         primitive_good = llt.LLTFormula(True, 1, llt.LE)
         primitive_bad = llt.LLTFormula(True, 0, llt.LE)
@@ -25,21 +29,30 @@ class TestSTL(unittest.TestCase):
 
         npt.assert_almost_equal(
             impurity.ext_inf_gain(
-                [1, 4, 0, 4.5], impurity.DEArgs(primitive_good, models, rho, traces, maxt)),
-            -0.6869615765973234)
+                [1, 4, 0, 4.5],
+                impurity.DEArgs(primitive_good, models, rho, traces, maxt),
+            ),
+            -0.6869615765973234,
+        )
 
         npt.assert_almost_equal(
             impurity.ext_inf_gain(
-                [1, 4, 0, 4.5], impurity.DEArgs(primitive_bad, models, rho, traces, maxt)),
-            0.0)
+                [1, 4, 0, 4.5],
+                impurity.DEArgs(primitive_bad, models, rho, traces, maxt),
+            ),
+            0.0,
+        )
 
     def test_inf_gain(self):
-        traces = inference.Traces([
-            [[1,2,3,4], [1,2,3,3.0], [1,2,3,4]],
-            [[1,2,3,4], [1,2,3,3.5], [1,2,3,4]],
-            [[1,2,3,4], [1,2,3,5.0], [1,2,3,4]],
-            [[1,2,3,4], [1,2,3,6.0], [1,2,3,4]]
-        ], [1, 1, -1, -1])
+        traces = inference.Traces(
+            [
+                [[1, 2, 3, 4], [1, 2, 3, 3.0], [1, 2, 3, 4]],
+                [[1, 2, 3, 4], [1, 2, 3, 3.5], [1, 2, 3, 4]],
+                [[1, 2, 3, 4], [1, 2, 3, 5.0], [1, 2, 3, 4]],
+                [[1, 2, 3, 4], [1, 2, 3, 6.0], [1, 2, 3, 4]],
+            ],
+            [1, 1, -1, -1],
+        )
         models = [llt.SimpleModel(signal) for signal in traces.signals]
         primitive_good = llt.LLTFormula(True, 1, llt.LE)
         primitive_bad = llt.LLTFormula(True, 0, llt.LE)
@@ -48,63 +61,62 @@ class TestSTL(unittest.TestCase):
 
         npt.assert_almost_equal(
             impurity.inf_gain(
-                [1, 4, 0, 4.5], impurity.DEArgs(primitive_good, models, rho, traces, maxt)),
-            -0.69314718056)
+                [1, 4, 0, 4.5],
+                impurity.DEArgs(primitive_good, models, rho, traces, maxt),
+            ),
+            -0.69314718056,
+        )
 
         npt.assert_almost_equal(
             impurity.inf_gain(
-                [1, 4, 0, 4.5], impurity.DEArgs(primitive_bad, models, rho, traces, maxt)),
-            0.0)
+                [1, 4, 0, 4.5],
+                impurity.DEArgs(primitive_bad, models, rho, traces, maxt),
+            ),
+            0.0,
+        )
 
     def test_ext_inf_gain2(self):
         traces = inference.Traces(
-            [[[-3.7972648],
-            [-1.22366531],
-            [ 0.        ]],
-
-            [[ 1.4999998 ],
-            [-1.22366532],
-            [ 0.        ]],
-
-            [[-0.49999975],
-            [-1.22366533],
-            [ 0.        ]]]
-            , [1, -1, -1])
+            [
+                [[-3.7972648], [-1.22366531], [0.0]],
+                [[1.4999998], [-1.22366532], [0.0]],
+                [[-0.49999975], [-1.22366533], [0.0]],
+            ],
+            [1, -1, -1],
+        )
         models = [llt.SimpleModel(signal) for signal in traces.signals]
         primitive_good = llt.LLTFormula(True, 0, llt.LE)
         primitive_bad = llt.LLTFormula(True, 1, llt.LE)
-        rho = np.array([1.0000000e+00, 3.3345704e-09, 1.0000000e+00])
+        rho = np.array([1.0000000e00, 3.3345704e-09, 1.0000000e00])
         maxt = 0
 
         gain_good = impurity.ext_inf_gain(
-            [0, 0, 0, -1.0],
-            impurity.DEArgs(primitive_good, models, rho, traces, maxt))
+            [0, 0, 0, -1.0], impurity.DEArgs(primitive_good, models, rho, traces, maxt)
+        )
         gain_bad = impurity.ext_inf_gain(
             [0, 0, 0, -1.22366],
-            impurity.DEArgs(primitive_bad, models, rho, traces, maxt))
+            impurity.DEArgs(primitive_bad, models, rho, traces, maxt),
+        )
 
         self.assertGreater(gain_bad, gain_good)
 
     def test_opt_inf_gain(self):
         traces = inference.Traces(
-            [[[-3.7972648],
-            [-1.22366531],
-            [ 0.        ]],
-
-            [[ 1.4999998 ],
-            [-1.22366532],
-            [ 0.        ]],
-
-            [[-0.49999975],
-            [-1.22366533],
-            [ 0.        ]]]
-            , [1, -1, -1])
-        rho = np.array([1.0000000e+00, 3.3345704e-09, 1.0000000e+00])
+            [
+                [[-3.7972648], [-1.22366531], [0.0]],
+                [[1.4999998], [-1.22366532], [0.0]],
+                [[-0.49999975], [-1.22366533], [0.0]],
+            ],
+            [1, -1, -1],
+        )
+        rho = np.array([1.0000000e00, 3.3345704e-09, 1.0000000e00])
         prims = llt.make_llt_primitives(traces.signals)
         # primitive_bad = llt.LLTFormula(False, 1, llt.LE)
 
-        opts = [impurity.optimize_impurity(traces, primitive.copy(), rho) for
-                primitive in prims]
+        opts = [
+            impurity.optimize_impurity(traces, primitive.copy(), rho)
+            for primitive in prims
+        ]
 
         for p, imp in opts:
             print("{} ({})".format(p, imp))
